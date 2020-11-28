3faa2f4ea5411182f8fcfe799bd5a66a57681d57
Averyanov Roman <averoma@debian-averoma.debian-averoma.com>
2020-11-28 12:04:35 +0700

    POST работает, надо изменить путь к директории на компе

--- Server.py
+++ Server.py
@@ -1,19 +1,48 @@
-from flask import Flask
-from flask import request, render_template
+import os
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = '/home/averoma/ITWORKS.sometimes/uploads'
+ALLOWED_EXTENSIONS = set(['mp3'])
+
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/uploaded')
+def uploaded():
+    return '''
+                <!doctype html>
+                <hrml>
+                <head> 
+                    <title>Spectrogram</title>
+                </head>
+                <body>
+                    
+                </body>
+                </html>
+                '''
+
+
+@app.route('/', methods=['GET', 'POST'])
 def render():
     if request.method == 'POST':
-        print("Rabotaet")
-        return "it fuckin works"
-    return render_template('index.html')
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        return redirect(url_for('uploaded'))
+
+    if request.method == 'GET':
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
