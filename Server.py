c9860e256103f848fb55f902a36c7e1e695ff25b
Pete <petra.lutsenko@mail.ru>
2020-11-29 10:49:28 +0700

    finish server

--- Server.py
+++ Server.py
@@ -6,7 +6,9 @@ import recognize
 
 UPLOAD_FOLDER = "./uploads"
 ALLOWED_EXTENSIONS = set(['mp3'])
-
+name = None
+message = None
+label = None
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -19,20 +21,18 @@ def allowed_file(filename):
 
 @app.route('/uploaded')
 def uploaded():
-    return render_template('load.html')
+    return render_template('result.html', song=name, status=message, label=label)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def render():
+    global name, message, label
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             name, message, label = recognize.check_song(filename)
-            print(name)
-            print(message)
-            print(label)
         return redirect(url_for('uploaded'))
 
     if request.method == 'GET':
